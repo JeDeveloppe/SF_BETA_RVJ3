f0af8f17bc52a4badc4da98cd7ebf0bc73fd7859
JeDeveloppe <jedeveloppe.contact@gmail.com>
2022-10-30 13:58:32 +0100

    ok template member

--- src/Controller/Member/AdresseController.php
+++ src/Controller/Member/AdresseController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\InformationsLegalesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -21,7 +22,13 @@ class AdresseController extends AbstractController
     /**
      * @Route("/new/{slug}/", name="app_adresse_new", methods={"GET", "POST"})
      */
-    public function new($slug, Request $request, AdresseRepository $adresseRepository, Security $security): Response
+    public function new(
+        $slug,
+        Request $request,
+        AdresseRepository $adresseRepository,
+        Security $security,
+        InformationsLegalesRepository $informationsLegalesRepository
+        ): Response
     {
         $user = $security->getUser();
  
@@ -31,10 +38,18 @@ class AdresseController extends AbstractController
 
         $adresse->setUser($user);
 
+        $array = [];
+
         if($slug == "facturation"){
             $adresse->setIsFacturation(true);
-        }else{
+            $array['titre'] = "facturation";
+        }else if($slug == "livraison"){
             $adresse->setIsFacturation(false);
+            $array['titre'] = "livraison";
+
+        }else{
+            $this->addFlash('danger', 'Url inconnue !');
+            return $this->redirectToRoute('app_member_adresses', [], Response::HTTP_SEE_OTHER);
         }
 
         $form = $this->createForm(AdresseType::class, $adresse, ['department' => $department]);
@@ -42,12 +57,16 @@ class AdresseController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $adresseRepository->add($adresse);
-            return $this->redirectToRoute('app_adresse_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Adresse créée !');
+            return $this->redirectToRoute('app_member_adresses', [], Response::HTTP_SEE_OTHER);
         }
+
 
         return $this->renderForm('member/adresse/new.html.twig', [
             'adresse' => $adresse,
-            'form' => $form
+            'form' => $form,
+            'informationsLegales' =>  $informationsLegalesRepository->findAll(),
+            'array' => $array
         ]);
     }
 
@@ -64,7 +83,13 @@ class AdresseController extends AbstractController
     /**
      * @Route("/{id}/edit", name="app_adresse_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Adresse $adresse, AdresseRepository $adresseRepository, Security $security): Response
+    public function edit(
+        Request $request,
+        Adresse $adresse,
+        AdresseRepository $adresseRepository,
+        Security $security,
+        InformationsLegalesRepository $informationsLegalesRepository
+        ): Response
     {
         $user = $security->getUser();
  
@@ -81,6 +106,7 @@ class AdresseController extends AbstractController
         return $this->renderForm('member/adresse/edit.html.twig', [
             'adresse' => $adresse,
             'form' => $form,
+            'informationsLegales' =>  $informationsLegalesRepository->findAll()
         ]);
     }
 
@@ -93,6 +119,6 @@ class AdresseController extends AbstractController
             $adresseRepository->remove($adresse);
         }
 
-        return $this->redirectToRoute('app_adresse_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_member_adresses', [], Response::HTTP_SEE_OTHER);
     }
 }
