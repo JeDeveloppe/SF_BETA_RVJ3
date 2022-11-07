6bbb7715af2053ba6dd277d5be7da615e72226d1
JeDeveloppe <jedeveloppe.contact@gmail.com>
2022-11-07 19:54:28 +0100

    gestion utilisateur/ admin ok

--- src/Form/AdminUserType.php
+++ src/Form/AdminUserType.php
@@ -8,15 +8,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class AdminUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'label' => 'Email:'
+            ])
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'ADMIN' => 'ROLE_ADMIN',
@@ -27,15 +31,25 @@ class AdminUserType extends AbstractType
                 'label' => 'Rôles'
             ])
             ->add('level')
-            ->add('password')
-            ->add('phone')
-            ->add('department')
-            ->add('createdAt')
-            ->add('nickname')
-            ->add('country', EntityType::class, [
-                'class' => Pays::class,
-                'choice_label' => 'name'
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe:',
+                'attr' => [
+                    'placeholder' => '********',
+                    'readonly' => true
+                ]
             ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone:'
+            ])
+            // ->add('department')
+            // ->add('createdAt')
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo:'
+            ])
+            // ->add('country', EntityType::class, [
+            //     'class' => Pays::class,
+            //     'choice_label' => 'name'
+            // ])
         ;
     }
 
