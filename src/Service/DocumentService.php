f00a7fd2e986f8e273d0503aade7c089884d3480
JeDeveloppe <jedeveloppe.contact@gmail.com>
2023-02-04 11:12:01 +0100

    dompdf

--- src/Service/DocumentService.php
+++ src/Service/DocumentService.php
@@ -2,19 +2,22 @@
 
 namespace App\Service;
 
+use Fpdf\Fpdf;
 use DateInterval;
+use Dompdf\Dompdf;
+use App\Entity\User;
+use Twig\Environment;
 use DateTimeImmutable;
 use App\Entity\Document;
 use App\Entity\DocumentLignes;
-use App\Repository\ConfigurationRepository;
-use App\Repository\DocumentLignesRepository;
 use App\Repository\UserRepository;
 use App\Repository\PanierRepository;
 use App\Repository\DocumentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\MethodeEnvoiRepository;
+use App\Repository\ConfigurationRepository;
+use App\Repository\DocumentLignesRepository;
 use App\Repository\InformationsLegalesRepository;
-use Fpdf\Fpdf;
 
 class DocumentService
 {
@@ -27,7 +30,8 @@ class DocumentService
         private UserRepository $userRepository,
         private PanierRepository $panierRepository,
         private ConfigurationRepository $configurationRepository,
-        private Utilities $utilities
+        private Utilities $utilities,
+        private Environment $templating
         )
     {
     }
@@ -216,17 +220,216 @@ class DocumentService
         return $setup['token'];
     }
 
-    public function factureToPdf($token){
+    // public function factureToPdf($token){
 
+    //     //on cherche le devis par le token
+    //     $doc = $this->documentRepository->findOneBy(['token' => $token]);
+
+    //     //on recupere le taux de tva
+    //     $tva = $doc->getTauxTva();
+
+    //     //on recupere les info legales
+    //     $infosLegales = $this->informationsLegalesRepository->findAll();
+    //     $rfj = $infosLegales[0];
+
+    //     //on recupere les occasions et les pieces detachees facturees
+    //     $occasions = $this->documentLignesRepository->findBy(['document' => $doc, 'boite' => null]);
+    //     $boites = $this->documentLignesRepository->findBy(['document' => $doc, 'occasion' => null]);
+
+    //     //ON FAIT LE TOTAL DES OCCASIONS
+    //     $totalOccasions = 0;
+    //     foreach($occasions as $occasion){
+    //         $totalOccasions = $totalOccasions + $occasion->getOccasion()->getPriceHt();
+    //     }
+
+    //     //ON FAIT LE TOTAL DES PIECES DETACHEES
+    //     $totalDetachees = 0;
+    //     foreach($boites as $boite){
+    //         $totalDetachees = $totalDetachees + $boite->getPrixVente();
+    //     }
+
+    //     // class PDF extends Fpdf
+    //     // {
+    //     //     // En-tête
+    //     //     function Header(){
+    //     //         // Logo
+    //     //         $this->Image('../images/design/refaitesvosjeux.png',10,6,30);
+    //     //         // Police Arial gras 15
+    //     //         $this->SetFont('Arial','B',15);
+    //     //         // Décalage à droite
+    //     //         $this->Cell(150);
+    //     //         // Titre
+    //     //         $this->Cell(30,10,'FACTURE',1,0,'C');
+    //     //         // Saut de ligne
+    //     //         $this->Ln(20);
+    //     //     }
+    
+    //     //     // Pied de page
+    //     //     function Footer(){
+    //     //         // Positionnement à 1,5 cm du bas
+    //     //         $this->SetY(-15);
+    //     //         // Police Arial italique 8
+    //     //         $this->SetFont('Arial','I',8);
+    //     //         // Numéro de page
+    //     //         $this->Cell(0,10,'Page '.$this->PageNo().'/1',0,0,'C');
+    //     //     }
+    
+    
+    //     // }
+
+
+    //      // Instanciation de la classe dérivée
+    //     $pdf = new Fpdf('P','mm','A4');
+    //     $pdf->AddPage();
+    //     $pdf->SetFont('Helvetica','',11);
+    //     $pdf->SetTextColor(0);
+
+    //     //HEADER
+    //     $pdf->Image('build/images/design/logoRVJ3.png',10,6,30);
+    //     // Police Arial gras 15
+    //     $pdf->SetFont('Arial','B',15);
+    //     // Décalage à droite
+    //     $pdf->Cell(150);
+    //     // Titre
+    //     $pdf->Cell(30,10,'FACTURE',1,0,'C');
+    //     // Saut de ligne
+    //     $pdf->Ln(20);
+
+    //     // Infos de la socièté sous le logo à gauche
+    //     $pdf->SetFont('Helvetica','',8);
+    //     $pdf->Text(10,35,utf8_decode('Adresse : '.$rfj->getAdresseSociete()));
+    //     $pdf->Text(10,40,'Siret : '.$rfj->getSiretSociete());
+
+    //     $pdf->SetFont('Helvetica','',11);
+    //     // Infos de la commande calées à gauche
+    //     $pdf->Text(8,78,utf8_decode('N° de facture : '.$doc->getNumeroFacture()));
+    //     $pdf->Text(8,84,'Date : '.$doc->getPaiement()->getTimeTransaction()->format('d-m-Y'));
+    //     $pdf->Text(8,90,utf8_decode('Mode de règlement : '.$doc->getPaiement()->getMoyenPaiement()));
+
+    //     // Infos du client calées à droite
+    //     $adresses = explode('<br/>',$doc->getAdresseFacturation());
+
+    //     $h = 38;
+    //     foreach($adresses as $adresse){
+    //         $pdf->Text(130,$h,utf8_decode($adresse));
+    //         $h += 8;
+    //     }
+  
+
+    //     //EN TETE DU TABLEAU
+    //     $position_entete_produits = 105;
+    //     // entete_table_accessoires($position_entete_produits);
+
+    //     $positionLigneAchat = 8;
+
+    //     if(count($occasions) > 0){
+    //         foreach($occasions as $ligneAchat){
+    //             $pdf->SetY($position_entete_produits + $positionLigneAchat);
+    //             $pdf->SetX(8);
+    //             $pdf->MultiCell(168,8,utf8_decode("Jeu d'occasion: ".$ligneAchat->getOccasion()->getBoite()->getNom().' - '.$ligneAchat->getOccasion()->getBoite()->getEditeur()),1,'C');
+    //             $pdf->SetY($position_entete_produits + $positionLigneAchat);
+    //             $pdf->SetX(176);
+    //             $pdf->MultiCell(24,8,number_format($ligneAchat->getOccasion()->getPriceHt() / 100 * $tva,2),1,'R');
+    //             $positionLigneAchat += 8;
+    //         }
+    //     }
+
+    //     $position_detail = $position_entete_produits + $positionLigneAchat; // Position à 9mm de l'entête
+
+    //     //LIGNE FOURNITURES
+    //     if(count($boites) > 0){
+    //         $pdf->SetY($position_detail);
+    //         $pdf->SetX(8);
+    //         $pdf->MultiCell(168,8,utf8_decode("Fourniture(s) de pièce(s)"),1,'C');
+    //         $pdf->SetY($position_detail);
+    //         $pdf->SetX(176);
+    //         $pdf->MultiCell(24,8,number_format($totalDetachees / 100,2),1,'R');
+    //     }else{
+    //         $position_detail -= 8;
+    //     }
+
+ 
+    //     //LIGNE LIVRAISON
+    //     $livraisons = explode('<br/>',$doc->getAdresseLivraison());
+
+    //     $destinationLivraisonOuRetrait = '';
+
+    //     foreach($livraisons as $livraison){
+    //         $destinationLivraisonOuRetrait .= $livraison.' ';
+    //     }
+
+    //     $pdf->SetY($position_detail + 16);
+    //     $pdf->SetX(8);
+    //     if($doc->getTotalLivraison() == 0){
+    //         $pdf->MultiCell(168,8,utf8_decode("RETRAIT: ".$destinationLivraisonOuRetrait),1,'R');
+    //     }else{
+    //         $pdf->MultiCell(168,8,utf8_decode("LIVRAISON: ".$destinationLivraisonOuRetrait),1,'R');
+    //     }
+    //     $pdf->SetY($position_detail + 16);
+    //     $pdf->SetX(176);
+    //     $pdf->MultiCell(24,8,number_format($doc->getTotalLivraison() / 100,2),1,'R');
+
+
+
+    //     //tableau des totaux
+    //     $tableauTotauxY = $position_detail + 50;
+    //     $pdf->SetY($tableauTotauxY);
+    //     $pdf->SetX(148);
+    //     $pdf->MultiCell(28,8,"Total HT:",1,'L');
+    //     $pdf->SetY($tableauTotauxY);
+    //     $pdf->SetX(176);
+    //     $pdf->MultiCell(24,8,number_format($doc->getTotalHT() / 100,"2",".",""),1,'R');
+    //     $pdf->SetY($tableauTotauxY + 8);
+    //     $pdf->SetX(148);
+    //     $pdf->MultiCell(28,8,"TVA:",1,'L');
+    //     $pdf->SetY($tableauTotauxY + 8);
+    //     $pdf->SetX(176);
+    //     $pdf->MultiCell(24,8,number_format($doc->getTotalTTC() / 100,"2",".",""),1,'R');
+    //     $pdf->SetY($tableauTotauxY + 16);
+    //     $pdf->SetX(148);
+    //     $pdf->MultiCell(28,8,"Total TTC:",1,'L');
+    //     $pdf->SetY($tableauTotauxY + 16);
+    //     $pdf->SetX(176);
+    //     $pdf->MultiCell(24,8,number_format($doc->getTotalTTC() / 100,"2",".",""),1,'R');
+
+    //     //LIGNE REMERCIEMENT
+    //     $pdf->SetFont('Helvetica','',12);
+    //     $pdf->SetY(250);
+    //     $pdf->SetX(10);
+    //     $pdf->MultiCell(190,8,utf8_decode("MERCI POUR VOTRE COMMANDE."),0,'C');
+
+    //     //ligne TVA dans table de config vaut 1 = PAS DE TVA
+    //     if($doc->getTauxTva() == 0){
+    //     $pdf->SetFont('Helvetica','',8);
+    //     $pdf->SetY(262);
+    //     $pdf->SetX(10);
+    //     $pdf->MultiCell(190,8,utf8_decode("TVA non applicable, article 293B du code général des impôts."),0,'C');
+    //     }
+
+    //     //FOOTER
+    //     // Positionnement à 1,5 cm du bas
+    //     $pdf->SetY(-35);
+    //     // Police Arial italique 8
+    //     $pdf->SetFont('Arial','I',8);
+    //     // Numéro de page
+    //     $pdf->Cell(0,10,'Page '.$pdf->PageNo().'/1',0,0,'C');
+
+
+    //     // Nom du fichier
+    //     $nom = 'RefaitesVosJeux-'.$doc->getNumeroFacture().'.pdf';
+    //     // Création du PDF
+    //     $pdf->Output($nom,'I');
+    // }
+
+    public function factureToPdf($token){
         //on cherche le devis par le token
         $doc = $this->documentRepository->findOneBy(['token' => $token]);
 
-        //on recupere le taux de tva
-        $tva = $doc->getTauxTva();
-
         //on recupere les info legales
-        $infosLegales = $this->informationsLegalesRepository->findAll();
-        $rfj = $infosLegales[0];
+        $infosLegales = $this->informationsLegalesRepository->findBy([]);
+
+        //on recupere le client
+        $user =  $doc->getUser();
 
         //on recupere les occasions et les pieces detachees facturees
         $occasions = $this->documentLignesRepository->findBy(['document' => $doc, 'boite' => null]);
@@ -244,176 +447,30 @@ class DocumentService
             $totalDetachees = $totalDetachees + $boite->getPrixVente();
         }
 
-        // class PDF extends Fpdf
-        // {
-        //     // En-tête
-        //     function Header(){
-        //         // Logo
-        //         $this->Image('../images/design/refaitesvosjeux.png',10,6,30);
-        //         // Police Arial gras 15
-        //         $this->SetFont('Arial','B',15);
-        //         // Décalage à droite
-        //         $this->Cell(150);
-        //         // Titre
-        //         $this->Cell(30,10,'FACTURE',1,0,'C');
-        //         // Saut de ligne
-        //         $this->Ln(20);
-        //     }
-    
-        //     // Pied de page
-        //     function Footer(){
-        //         // Positionnement à 1,5 cm du bas
-        //         $this->SetY(-15);
-        //         // Police Arial italique 8
-        //         $this->SetFont('Arial','I',8);
-        //         // Numéro de page
-        //         $this->Cell(0,10,'Page '.$this->PageNo().'/1',0,0,'C');
-        //     }
-    
-    
-        // }
+        $html = $this->templating->render("commun/template_facture.html.twig", [
+            'doc' => $doc,
+            'infosLegales' => $infosLegales,
+            'user' => $user,
+            'occasions' => $occasions,
+            'boites' => $boites,
+            'totalOccasions' => $totalOccasions,
+            'totalDetachees' => $totalDetachees
+        ]);
 
+        $html .= '<style>'.file_get_contents("../public/build/site.95d9ef6a.css").'</style>';
+   
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
 
-         // Instanciation de la classe dérivée
-        $pdf = new Fpdf('P','mm','A4');
-        $pdf->AddPage();
-        $pdf->SetFont('Helvetica','',11);
-        $pdf->SetTextColor(0);
+        $dompdf->loadHtml($html);
 
-        //HEADER
-        $pdf->Image('build/images/design/logoRVJ3.png',10,6,30);
-        // Police Arial gras 15
-        $pdf->SetFont('Arial','B',15);
-        // Décalage à droite
-        $pdf->Cell(150);
-        // Titre
-        $pdf->Cell(30,10,'FACTURE',1,0,'C');
-        // Saut de ligne
-        $pdf->Ln(20);
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'portrait');
 
-        // Infos de la socièté sous le logo à gauche
-        $pdf->SetFont('Helvetica','',8);
-        $pdf->Text(10,35,utf8_decode('Adresse : '.$rfj->getAdresseSociete()));
-        $pdf->Text(10,40,'Siret : '.$rfj->getSiretSociete());
+        // Render the HTML as PDF
+        $dompdf->render();
 
-        $pdf->SetFont('Helvetica','',11);
-        // Infos de la commande calées à gauche
-        $pdf->Text(8,78,utf8_decode('N° de facture : '.$doc->getNumeroFacture()));
-        $pdf->Text(8,84,'Date : '.$doc->getPaiement()->getTimeTransaction()->format('d-m-Y'));
-        $pdf->Text(8,90,utf8_decode('Mode de règlement : '.$doc->getPaiement()->getMoyenPaiement()));
-
-        // Infos du client calées à droite
-        $adresses = explode('<br/>',$doc->getAdresseFacturation());
-
-        $h = 38;
-        foreach($adresses as $adresse){
-            $pdf->Text(130,$h,utf8_decode($adresse));
-            $h += 8;
-        }
-  
-
-        //EN TETE DU TABLEAU
-        $position_entete_produits = 105;
-        // entete_table_accessoires($position_entete_produits);
-
-        $positionLigneAchat = 8;
-
-        if(count($occasions) > 0){
-            foreach($occasions as $ligneAchat){
-                $pdf->SetY($position_entete_produits + $positionLigneAchat);
-                $pdf->SetX(8);
-                $pdf->MultiCell(168,8,utf8_decode("Jeu d'occasion: ".$ligneAchat->getOccasion()->getBoite()->getNom().' - '.$ligneAchat->getOccasion()->getBoite()->getEditeur()),1,'C');
-                $pdf->SetY($position_entete_produits + $positionLigneAchat);
-                $pdf->SetX(176);
-                $pdf->MultiCell(24,8,number_format($ligneAchat->getOccasion()->getPriceHt() / 100 * $tva,2),1,'R');
-                $positionLigneAchat += 8;
-            }
-        }
-
-        $position_detail = $position_entete_produits + $positionLigneAchat; // Position à 9mm de l'entête
-
-        //LIGNE FOURNITURES
-        if(count($boites) > 0){
-            $pdf->SetY($position_detail);
-            $pdf->SetX(8);
-            $pdf->MultiCell(168,8,utf8_decode("Fourniture(s) de pièce(s)"),1,'C');
-            $pdf->SetY($position_detail);
-            $pdf->SetX(176);
-            $pdf->MultiCell(24,8,number_format($totalDetachees / 100,2),1,'R');
-        }else{
-            $position_detail -= 8;
-        }
-
- 
-        //LIGNE LIVRAISON
-        $livraisons = explode('<br/>',$doc->getAdresseLivraison());
-
-        $destinationLivraisonOuRetrait = '';
-
-        foreach($livraisons as $livraison){
-            $destinationLivraisonOuRetrait .= $livraison.' ';
-        }
-
-        $pdf->SetY($position_detail + 16);
-        $pdf->SetX(8);
-        if($doc->getTotalLivraison() == 0){
-            $pdf->MultiCell(168,8,utf8_decode("RETRAIT: ".$destinationLivraisonOuRetrait),1,'R');
-        }else{
-            $pdf->MultiCell(168,8,utf8_decode("LIVRAISON: ".$destinationLivraisonOuRetrait),1,'R');
-        }
-        $pdf->SetY($position_detail + 16);
-        $pdf->SetX(176);
-        $pdf->MultiCell(24,8,number_format($doc->getTotalLivraison() / 100,2),1,'R');
-
-
-
-        //tableau des totaux
-        $tableauTotauxY = $position_detail + 50;
-        $pdf->SetY($tableauTotauxY);
-        $pdf->SetX(148);
-        $pdf->MultiCell(28,8,"Total HT:",1,'L');
-        $pdf->SetY($tableauTotauxY);
-        $pdf->SetX(176);
-        $pdf->MultiCell(24,8,number_format($doc->getTotalHT() / 100,"2",".",""),1,'R');
-        $pdf->SetY($tableauTotauxY + 8);
-        $pdf->SetX(148);
-        $pdf->MultiCell(28,8,"TVA:",1,'L');
-        $pdf->SetY($tableauTotauxY + 8);
-        $pdf->SetX(176);
-        $pdf->MultiCell(24,8,number_format($doc->getTotalTTC() / 100,"2",".",""),1,'R');
-        $pdf->SetY($tableauTotauxY + 16);
-        $pdf->SetX(148);
-        $pdf->MultiCell(28,8,"Total TTC:",1,'L');
-        $pdf->SetY($tableauTotauxY + 16);
-        $pdf->SetX(176);
-        $pdf->MultiCell(24,8,number_format($doc->getTotalTTC() / 100,"2",".",""),1,'R');
-
-        //LIGNE REMERCIEMENT
-        $pdf->SetFont('Helvetica','',12);
-        $pdf->SetY(250);
-        $pdf->SetX(10);
-        $pdf->MultiCell(190,8,utf8_decode("MERCI POUR VOTRE COMMANDE."),0,'C');
-
-        //ligne TVA dans table de config vaut 1 = PAS DE TVA
-        if($doc->getTauxTva() == 0){
-        $pdf->SetFont('Helvetica','',8);
-        $pdf->SetY(262);
-        $pdf->SetX(10);
-        $pdf->MultiCell(190,8,utf8_decode("TVA non applicable, article 293B du code général des impôts."),0,'C');
-        }
-
-        //FOOTER
-        // Positionnement à 1,5 cm du bas
-        $pdf->SetY(-35);
-        // Police Arial italique 8
-        $pdf->SetFont('Arial','I',8);
-        // Numéro de page
-        $pdf->Cell(0,10,'Page '.$pdf->PageNo().'/1',0,0,'C');
-
-
-        // Nom du fichier
-        $nom = 'RefaitesVosJeux-'.$doc->getNumeroFacture().'.pdf';
-        // Création du PDF
-        $pdf->Output($nom,'I');
+        // Output the generated PDF to Browser
+        $dompdf->stream("Facture-".$doc->getNumeroFacture().".pdf");
     }
 }
